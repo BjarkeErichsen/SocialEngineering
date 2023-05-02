031dc63a79b18977bfc42fbe2bc3d4ac1ca1d188
AugustBirch <augbirch@gmail.com>
2023-05-02 15:31:27 +0200

    Added country to txt with names

--- Project/wiki_extractor.py
+++ Project/wiki_extractor.py
@@ -8,6 +8,42 @@ import pickle
 from urllib.parse import urlparse
 import multiprocessing as mp
 
+from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer
+
+countries =  ['United States (U.S.A)', 'United Kingdom (U.K.)', 'Germany', 'France', 'Japan', 'Russia (USSR)',
+              'Canada', 'Italy', 'Switzerland', 'Sweden', 'Australia', 'Netherlands', 'China',
+              'South Korea', 'Israel', 'Denmark', 'Spain', 'Belgium', 'Austria', 'Norway',
+              'Finland', 'India', 'Brazil', 'Mexico', 'Poland', 'Turkey', 'Czech Republic',
+              'Portugal', 'Greece', 'Hungary', 'Ireland', 'Taiwan', 'Argentina', 'Romania',
+              'Serbia', 'Singapore', 'Croatia', 'Slovenia', 'Ukraine', 'New Zealand', 'Bulgaria',
+              'Egypt', 'Iran', 'Jordan', 'Kazakhstan', 'Malaysia', 'Morocco', 'Saudi Arabia',
+              'South Africa', 'Thailand', 'Tunisia', 'Vietnam', 'Bangladesh', 'Chile', 'Colombia',
+              'Indonesia', 'Kenya', 'Nigeria', 'Pakistan', 'Peru']
+def get_wikipedia_article_text(title, language='en'):
+    base_url = f'https://{language}.wikipedia.org/w/api.php'
+
+    params = {
+        'action': 'query',
+        'format': 'json',
+        'prop': 'extracts',
+        'exlimit': '1',
+        'explaintext': '1',
+        'titles': title
+    }
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    page_id = list(data['query']['pages'].keys())[0]
+
+    if page_id == '-1':
+        print('Article not found.')
+        return None
+
+    text = data['query']['pages'][page_id]['extract']
+    return text
+
 def save_dict_txt(dictionary_to_save, text_name):
     with open(text_name, 'w', encoding='utf-8') as file:
         json.dump(dictionary_to_save, file, ensure_ascii=False)
@@ -27,6 +63,7 @@ def load_dict_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         my_dict = json.load(file)
     return my_dict
+
 def find_wiki_page_id(title):
     url = "https://en.wikipedia.org/w/api.php"
     session = requests.Session()
@@ -92,6 +129,37 @@ def count_occurrences(lst):
             counts[elem] = 1
     return [(elem, counts[elem]) for elem in counts]
 
+def extract_country(string):
+
+    end = string.find('(')
+    if end == -1:
+        end = None
+    start = string.find('–')
+    if start == -1:
+        start = string.find('--')
+        if start == -1:
+            start = string.find('- ')
+            if start == -1:
+                start = 0
+    return str(string[start+2:end]), start, end
+def generate_embedding_list(list):
+    embedding_list = []
+    for i in list:
+        embedding_i = model.encode(i)
+        embedding_list.append(embedding_i)
+    return embedding_list
+
+def find_most_similar(subject, embedding_list):
+    embedding_subject = model.encode(subject)
+    most_similar = None
+    max_score = 0
+    for e, embedding in enumerate(embedding_list):
+        similarity = cosine_similarity([embedding_subject], [embedding])
+        if similarity>max_score:
+            max_score = similarity
+            most_similar = e
+    return most_similar
+
 def extract_names_from_list():
     # Specify the URL and parameters for the API request
     url = "https://en.wikipedia.org/w/api.php"
@@ -107,6 +175,19 @@ def extract_names_from_list():
 
     # Extract the links from the API response
     title_list = []
+
+    article_title = 'List_of_physicists'
+    article_text = get_wikipedia_article_text(article_title)
+    text_list = article_text.splitlines(True)
+    text_list = [s.replace("\n", "") for s in text_list]
+    text_list = [s for s in text_list if "=" not in s]
+    text_list = list(filter(None, text_list))
+    text_list = text_list[1:]
+    text_list = text_list[:-3]
+
+    embedding_list_wiki = generate_embedding_list(text_list)
+    embedding_list_countries = generate_embedding_list(countries)
+
     if response.status_code == 200:
         data = response.json()
         if "links" in data["parse"]:
@@ -124,12 +205,24 @@ def extract_names_from_list():
                     data_json = link_data["query"]["pages"]
                     pageid = next(iter(data_json))
                     title = data_json[pageid]['title']
-                    title_list.append((pageid, title))
 
-    with open('titles_ids.txt', 'w', encoding='utf-8') as f:
-        for pageid, title in title_list:
+                    most_similar_line_in_wiki_id = find_most_similar(title,embedding_list_wiki)
+                    most_similar_line_in_wiki = text_list[most_similar_line_in_wiki_id]
+                    country_estimate = extract_country(most_similar_line_in_wiki)[0]
+
+                    if country_estimate in countries:
+                        title_list.append((pageid, title, country_estimate))
+
+                    else:
+                        most_similar_country_id = find_most_similar(country_estimate, embedding_list_countries)
+                        most_similar_country = countries[most_similar_country_id]
+                        title_list.append((pageid, title, most_similar_country))
+
+
+    with open('real_titles_ids.txt', 'w', encoding='utf-8') as f:
+        for pageid, title, country in title_list:
             if pageid != -1:
-                f.write(f"{pageid},{title}\n")
+                f.write(f"{pageid},{title},{country}\n")
 
 def get_physicist_social_network(name, base_url, id_to_name_dict, list_valid_ids):
     title_list = []
@@ -171,7 +264,6 @@ def get_physicist_social_network(name, base_url, id_to_name_dict, list_valid_ids
 
     return (name, count_occurrences(title_list))
 
-
 def the_social_network(input_txt, output_txt):
     influence_dict = {}
     id_to_name_dict = load_dict_txt(input_txt)
@@ -199,9 +291,21 @@ def the_social_network(input_txt, output_txt):
     with open(output_txt, 'w', encoding='utf-8') as file:
         json.dump(influence_dict, file, ensure_ascii=False)
 
-
 if __name__ == '__main__':
-    the_social_network("real_titles_ids.txt", "social_network.txt")
-    social_network = load_dict_json("social_network.txt")
-    print(social_network)
+    # the_social_network("real_titles_ids.txt", "social_network.txt")
+    # social_network = load_dict_json("social_network.txt")
+    # print(social_network)
+
+    model = SentenceTransformer('sentence-transformers/paraphrase-distilroberta-base-v1')
+    country1 = "Syracuse, Greece"
+
+    extract_names_from_list()
+    max_score = 0
+    most_similar_country = None
+
+
+
+    # print(most_similar_country)
+    # print(similarity)
+
 
